553bc7b333fe9a1574366037b699cf3e728e23b7
Thiago Sant'Helena <thiago.sant.helena@gmail.com>
2018-12-19 16:27:10 -0200

    First commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+from sensitive import PAT, VERIFY_TOKEN, ACCESS_TOKEN
+from pymessenger.bot import Bot
+import requests
+
+
+app = Flask(__name__)
+bot = Bot(ACCESS_TOKEN)
+
+# Dealing with GET method for verification
+
+
+@app.route('/', methods=['GET', ])
+def hanlde_verification():
+    if request.args.get('hub.verify_token', '') == VERIFY_TOKEN:
+        return request.args.get('hub.challenge', '')
+
+    else:
+        return 'Error, wrong verification token'
+
+
+# Dealing with POST method for messaging
+@app.route('/', methods=['POST', ])
+def handle_message():
+    input_data = request.get_json()
+
+    for event in input_data['entry']:
+        messaging = event['messaging']
+        for message in messaging:
+            recipient_id = message['sender']['id']
+
+            send_message(recipient_id, get_response_message())
+
+
+def send_message(recipient_id, response):
+    bot.send_text_message(recipient_id, response)
+
+
+def get_response_message():
+    return ('Hello!')
+
+
+if __name__ == '__main__':
+    app.run()
