0488c51979636cc0e286b1cae2a7539143e7108a
Thiago Sant'Helena <thiago.sant.helena@gmail.com>
2018-12-20 11:57:48 -0200

    Pass tokens to environ

--- app.py
+++ app.py
@@ -10,29 +10,46 @@ bot = Bot(os.environ['ACCESS_TOKEN'])
 # Dealing with GET method for verification
 
 
-@app.route('/', methods=['GET', ])
-def hanlde_verification():
-    if request.args.get('hub.verify_token', '') == os.environ['VERIFY_TOKEN']:
-        return request.args.get('hub.challenge', '')
+@app.route('/', methods=['GET', 'POST'])
+def hanlde():
+    print('CALL!')
+    if request.method == 'GET':
+        print('GET')
+        if request.args.get('hub.verify_token', '') == os.environ['VERIFY_TOKEN']:
+            return request.args.get('hub.challenge', '')
+
+        else:
+            return 'Error, wrong verification token'
 
     else:
-        return 'Error, wrong verification token'
+        print('POST')
+        input_data = request.get_json()
+
+        print('input_data', input_data)
+
+        for event in input_data['entry']:
+            messaging = event['messaging']
+            for message in messaging:
+                print(message['message'].get('text'))
+                recipient_id = message['sender']['id']
+
+                send_message(recipient_id, get_response_message())
 
 
 # Dealing with POST method for messaging
-@app.route('/', methods=['POST', ])
-def handle_message():
-    input_data = request.get_json()
+# @app.route('/', methods=['POST', ])
+# def handle_message():
+#     input_data = request.get_json()
 
-    print('input_data', input_data)
+#     print('input_data', input_data)
 
-    for event in input_data['entry']:
-        messaging = event['messaging']
-        for message in messaging:
-            print(message['message'].get('text'))
-            recipient_id = message['sender']['id']
+#     for event in input_data['entry']:
+#         messaging = event['messaging']
+#         for message in messaging:
+#             print(message['message'].get('text'))
+#             recipient_id = message['sender']['id']
 
-            send_message(recipient_id, get_response_message())
+#             send_message(recipient_id, get_response_message())
 
 
 def send_message(recipient_id, response):
